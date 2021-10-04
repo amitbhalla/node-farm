fbd5bcce2d0005f6dad05c8629455f2b4bd5ed88
amitbhalla <amit9815@gmail.com>
2021-10-04 10:22:56 +0530

    Basic routing

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer(
     (req, res) => {
-        res.end("Hello from the server!")
+        console.log(req.url);
+        const pathName = req.url;
+        if (pathName === "/" || pathName === "/overview") {
+            res.end("This is the overview page!");
+        }
+        else if (pathName === "/product") {
+            res.end("This is the product page!");
+        }
     }
 );
 
-server.listen(3000, "0.0.0.0",
-    () => { console.log("Listening to requests from 0.0.0.0 on port 3000"); }
+server.listen(
+    3000,
+    "0.0.0.0",
+    () => {
+        console.log("Listening to requests from 0.0.0.0 on port 3000");
+    }
 );
