1d6ea21ad32c0030a5cc6e3b6193de69d498a8e1
amitbhalla <amit9815@gmail.com>
2021-10-04 14:45:55 +0530

    NPMEnabled

--- nodefarm.js
+++ nodefarm.js
@@ -1,32 +1,15 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+import { createServer } from "http";
+import { readFileSync } from "fs";
+import replaceTemplate from "./replaceTemplate.js";
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+const tempOverview = readFileSync("./templates/template-overview.html", "utf-8");
+const tempCard = readFileSync("./templates/template-card.html", "utf-8");
+const tempProduct = readFileSync("./templates/template-product.html", "utf-8");
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = readFileSync("./dev-data/data.json", "utf-8");
 const dataObj = JSON.parse(data);
 
-
-const replaceTemplate = (template, product) => {
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-    }
-    return output;
-};
-
-
-const server = http.createServer(
+const server = createServer(
     (req, res) => {
         const baseURL = `http://${req.headers.host}`;
         console.log(baseURL);
@@ -66,7 +49,5 @@ const server = http.createServer(
 
 server.listen(
     3000, "0.0.0.0",
-    () => {
-        console.log("Listening to port 3000 on 0.0.0.0 ...");
-    },
+    () => { console.log("Listening to port 3000 on 0.0.0.0 ..."); },
 );
